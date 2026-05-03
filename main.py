b5a75802dc42055ac8d70bce8184967e76d4fe14
Riane <rlgguico@gmail.com>
2026-05-03 22:14:23 +0100

    minor changes - next step: enforce piece movement rule

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ class Game:
         self.square_selected = ()
         # where to move selected piece
         self.player_clicks = []
-        # piece selected by user
 
     def draw_game_window(self):
         self.window.fill((26, 26, 26))
@@ -28,6 +27,33 @@ class Game:
     def run_game(self):
         pass
 
+    def move_pieces(self):
+        coordinates = pygame.mouse.get_pos()
+        col = coordinates[0] // SQUARE_SIZE
+        row = coordinates[1] // SQUARE_SIZE
+        if 0 <= row < 8 and 0 <= col < 8:
+            if self.square_selected == (row, col): # player seleted same square twice
+                self.square_selected = ()
+                self.player_clicks = []
+            else:
+                self.square_selected = (row, col)
+                self.player_clicks.append(self.square_selected)
+    
+            if len(self.player_clicks) == 2:
+                start_row, start_col = self.player_clicks[0]
+                end_row, end_col = self.player_clicks[1]
+
+                piece = self.board.board[start_row][start_col]
+                
+                if piece:
+                    piece.row = end_row
+                    piece.col = end_col
+
+                    self.board.board[end_row][end_col] = piece
+                    self.board.board[start_row][start_col] = None
+            
+                self.square_selected = ()
+                self.player_clicks = []
 
     def game_loop(self):
         while True:
@@ -43,32 +69,7 @@ class Game:
                         pygame.quit()
                         sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    coordinates = pygame.mouse.get_pos()
-                    col = coordinates[0] // SQUARE_SIZE
-                    row = coordinates[1] // SQUARE_SIZE
-                    if 0 <= row < 8 and 0 <= col < 8:
-                        if self.square_selected == (row, col): # player seleted same square twice
-                            self.square_selected = ()
-                            self.player_clicks = []
-                        else:
-                            self.square_selected = (row, col)
-                            self.player_clicks.append(self.square_selected)
-                    
-                        if len(self.player_clicks) == 2:
-                            start_row, start_col = self.player_clicks[0]
-                            end_row, end_col = self.player_clicks[1]
-
-                            piece = self.board.board[start_row][start_col]
-
-                            if piece:
-                                piece.row = end_row
-                                piece.col = end_col
-
-                                self.board.board[end_row][end_col] = piece
-                                self.board.board[start_row][start_col] = None
-            
-                            self.square_selected = ()
-                            self.player_clicks = []
+                    self.move_pieces()
 
             self.run_game()
             self.draw_game_window()
