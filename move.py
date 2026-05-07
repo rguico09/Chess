fc46a0e593fa4d602e9cc64f64e174e3c463dd9b
Riane <rlgguico@gmail.com>
2026-05-07 20:28:56 +0100

    Added move class to a separate file

---
+++ move.py
@@ -0,0 +1,8 @@
+class Move:
+    def __init__(self, start_sq, end_sq, board):
+        self.start_row = start_sq[0]
+        self.start_col = start_sq[1]
+        self.end_row = end_sq[0]
+        self.end_col = end_sq[1]
+        self.piece_moved = board[self.start_row][self.start_col]
+        self.piece_captured = board[self.end_row][self.end_col]
